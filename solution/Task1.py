34bf22c6b824efbbbad173c5fcfc65b3d15fdb5b
Architect <greatraksin@icloud.com>
2019-11-02 13:12:37 +0300

    Adding tasks

---
+++ solution/Task1.py
@@ -0,0 +1,17 @@
+def positive():
+    print("Положительное")
+
+
+def negative():
+    print("Отрицательное")
+
+
+def test():
+    a = int(input())
+    if a > 0:
+        positive()
+    elif a < 0:
+        negative()
+
+
+test()
